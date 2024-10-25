3054add73c4fae297869befc80c3b7423ec30dcc
Addison Adcock <89235734+AddisonAdcock@users.noreply.github.com>
2024-10-25 10:08:56 -0500

    Create code file

---
+++ code_me_something_that_wishes__code.py
@@ -0,0 +1,13 @@
+Sure, here is a simple Python script that wishes the user a happy birthday:
+
+```python
+# This script asks for the user's name and wishes them a happy birthday
+
+# Ask for the user's name
+name = input("Please enter your name: ")
+
+# Print a happy birthday message
+print("Happy Birthday, " + name + "!")
+```
+
+In this script, the `input()` function is used to get the user's name, and then this name is used in the `print()` function to display a personalized happy birthday message.
